480b092a12b6953ff5467772f7d13739039ffc29
Dustin <dustinwaggoner@comcast.net>
2016-05-20 11:11:37 -0700

    json api not working

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160517043822) do
+ActiveRecord::Schema.define(version: 20160520180156) do
 
   create_table "items", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20160517043822) do
     t.string   "full_name"
     t.string   "email"
     t.string   "bio"
+    t.string   "password"
   end
 
 end

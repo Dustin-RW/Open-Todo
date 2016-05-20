12e14843ffa056530ce5594140ed41bdc8e78185
Dustin <dustinwaggoner@comcast.net>
2016-05-19 17:01:01 -0700

    serializers established and rendering json

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'faker'
+include Faker
+
+50.times do
+
+  User.create!(
+    email: "#{Internet.email}",
+    bio: "#{Hipster.paragraph(3, true, 4)}",
+    full_name: "#{Name.name}"
+  )
+
+end
+
+puts "Seeds Finished"
+puts "#{User.count} users created"

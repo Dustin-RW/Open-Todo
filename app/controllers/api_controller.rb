3e0ef1f921d7d834391572ac2b999ee320a5434b
Dustin <dustinwaggoner@comcast.net>
2016-05-22 22:07:30 -0700

    curl command rendering json via command line now, edited api_controller

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -8,7 +8,7 @@ class ApiController < ApplicationController
    # authenticate_or_request_with_http_basic implements basic HTTP authentication, which ensures HTTP requests are accompanied by a valid username and password
 
    def authenticated?
-     authenticate_or_request_with_http_basic {|username, password| User.where( username: username, password: password).present? }
+     authenticate_or_request_with_http_basic {|full_name, password| User.where( full_name: full_name, password: password).present? }
    end
 
 end
